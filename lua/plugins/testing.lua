93644593579953e94e7919335272dc917f93034d
humankernel <rivasjoaquin02@gmail.com>
2025-05-01 11:01:54 -0400

    feat: test with competitest

--- lua/plugins/testing.lua
+++ lua/plugins/testing.lua
@@ -1,9 +1,25 @@
+-- competitest.nvim is a testcase manager and checker
+-- see: `https://github.com/xeluxee/competitest.nvim`
 return {
-    'xeluxee/competitest.nvim', lazy = true, event = "VeryLazy",
-    opts = {
-        compile_command = { 'g++', '-std=c++17', '-O2', '-o', 'output', 'main.cpp' },
-    },
+    "xeluxee/competitest.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
     keys = {
-        { '<F5>', ':w <CR> :!g++ -std=c++17 -O2 -o %< % <CR> :!./%< <CR>', desc = "Compile Current Buffer" },
+        { "<leader>tr", ":CompetiTest run <CR>", desc = "[T]est [R]un" },
+        {
+            "<leader>tn",
+            ":CompetiTest add_testcase <CR>",
+            desc = "[T]est [A]dd",
+        },
+        {
+            "<leader>td",
+            ":CompetiTest delete_testcase <CR>",
+            desc = "[T]est [D]elete",
+        },
+        {
+            "<leader>te",
+            ":CompetiTest edit_testcase <CR>",
+            desc = "[T]est [E]dit",
+        },
     },
+    opts = {},
 }
