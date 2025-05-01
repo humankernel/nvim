1b7029cd0448fe653e5ee4d82b35373abdebb4c8
humankernel <rivasjoaquin02@gmail.com>
2025-05-01 10:59:30 -0400

    feat: native lsp

--- init.lua
+++ init.lua
@@ -1,13 +1,2 @@
 require "config.options"
 require "config.lazy"
-
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
-vim.lsp.enable({
-  "luals",
-  "gopls",
-  "clangd",
-  -- python
-  "pyright",
-  "ruff",
-})
-
