0ea6392160027951bc8c0ef8191b65d1a6cd474c
humankernel <rivasjoaquin02@gmail.com>
2025-05-01 11:01:07 -0400

    feat: sql ui

---
+++ lua/plugins/sql.lua
@@ -0,0 +1,20 @@
+return {
+  "kristijanhusak/vim-dadbod-ui",
+  enable = true,
+  lazy = true,
+  event = "VeryLazy",
+  dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { 'sql', 'plsql' }, lazy = true },
+  },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
+}
